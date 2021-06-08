1faa50dd56262f1dc161eb8acb32b51d47c5ca1a
Axel Anaya <axel_anaya@hotmail.com>
2021-06-07 19:39:15 -0500

    :wrench: Añadido modulo de configuraciones

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    process.env.NODE_ENV !== 'production'
+      ? ConfigModule.forRoot({
+          envFilePath: `.env.${process.env.NODE_ENV}`,
+        })
+      : null,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
