ce4f2722bd1f7f8d9e04851200ee8658b6661ffd
Axel Anaya <axel_anaya@hotmail.com>
2021-06-09 20:57:37 -0500

    :truck: Cambio en el import de ProductModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { ProductoModule } from '@producto/producto.module';
+import { ProductoModule } from './producto/producto.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
