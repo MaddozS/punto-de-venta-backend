559b28880a9503fb9a1f137e5c3e9f1562fdd77b
Axel Anaya <axel_anaya@hotmail.com>
2021-06-09 20:40:12 -0500

    :sparkles: Creado DTO de Producto

---
+++ src/producto/dto/producto.dto.ts
@@ -0,0 +1,7 @@
+export class CreateProductoDTO {
+  readonly nombre: string;
+  readonly descripcion: string;
+  readonly precio: number;
+  readonly cantidad: number;
+  readonly agregadoEl: Date;
+}
