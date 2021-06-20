3aabfebd7ac88e3d9e53908d9ca4394fd6c30df2
Axel Anaya <axel_anaya@hotmail.com>
2021-06-19 21:50:25 -0500

    :sparkles: Agregado y actualizado endpoints al controlador de Product

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Post, Get, HttpStatus, Body, BadRequestException, HttpCode } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  HttpStatus,
+  Body,
+  BadRequestException,
+  HttpCode,
+  Param,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { CreateProductDTO } from './dto/product.dto';
 import { Product } from './schemas/product.schema';
 import { ProductService } from './product.service';
@@ -8,21 +19,42 @@ export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Get()
-  async getProduct(): Promise<Product[]> {
-    const products = await this.productService.findAll();
+  async getProducts(): Promise<Product[]> {
+    const products = await this.productService.getAll();
+
+    return products;
+  }
+
+  @Get(':id')
+  async getProduct(@Param() params): Promise<Product> {
+    const products = await this.productService.getOne(params.id);
 
     return products;
   }
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
-  async create(@Body() createProductDTO: CreateProductDTO): Promise<Product> {
+  async createProduct(@Body() createProductDTO: CreateProductDTO): Promise<Product> {
     let product: Product;
 
-    product = await this.productService.create(createProductDTO).catch((error) => {
+    product = await this.productService.createOne(createProductDTO).catch((error) => {
       throw new BadRequestException(error, 'No se ha podido crear el producto');
     });
 
     return product;
+  }
+
+  @Put(':id')
+  async updateProduct(@Param() params, @Body() createProductDTO: CreateProductDTO): Promise<Product> {
+    const products = await this.productService.updateOne(params.id, createProductDTO);
+
+    return products;
+  }
+
+  @Delete(':id')
+  async deleteProduct(@Param() params): Promise<Product> {
+    const products = await this.productService.deleteOne(params.id);
+
+    return products;
   }
 }
