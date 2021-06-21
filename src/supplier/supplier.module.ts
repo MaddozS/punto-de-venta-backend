288dc99ce6bdf4d7be6a7ef7e3fcc9d47a961864
Axel Anaya <axel_anaya@hotmail.com>
2021-06-21 17:39:02 -0500

    :sparkles: Agregado módulo mongoose al módulo supplier

--- src/supplier/supplier.module.ts
+++ src/supplier/supplier.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SupplierService } from './supplier.service';
 import { SupplierController } from './supplier.controller';
+import { Supplier, SupplierSchema } from './schemas/supplier.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Supplier.name, schema: SupplierSchema }])],
   providers: [SupplierService],
-  controllers: [SupplierController]
+  controllers: [SupplierController],
 })
 export class SupplierModule {}
