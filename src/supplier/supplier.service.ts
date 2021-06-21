6f36876dee2a161b78ce1f2ae01724dc1745fe13
Axel Anaya <axel_anaya@hotmail.com>
2021-06-21 17:27:03 -0500

    :sparkles: Implementado interfaz de Service en el servicio de Supplier

--- src/supplier/supplier.service.ts
+++ src/supplier/supplier.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Service } from 'src/types/Service';
+import { CreateSupplierDTO } from './dto/supplier.dto';
+import { Supplier, SupplierDocument } from './schemas/supplier.schema';
 
 @Injectable()
-export class SupplierService {}
+export class SupplierService implements Service<Supplier, CreateSupplierDTO> {
+  constructor(@InjectModel(Supplier.name) private supplierModel: Model<SupplierDocument>) {}
+  getAll(): Promise<Supplier[]> {
+    throw new Error('Method not implemented.');
+  }
+  getOne(id: string): Promise<Supplier> {
+    throw new Error('Method not implemented.');
+  }
+  createOne(dto: CreateSupplierDTO): Promise<Supplier> {
+    throw new Error('Method not implemented.');
+  }
+  updateOne(id: string, dto: CreateSupplierDTO): Promise<Supplier> {
+    throw new Error('Method not implemented.');
+  }
+  deleteOne(id: string): Promise<Supplier> {
+    throw new Error('Method not implemented.');
+  }
+}
