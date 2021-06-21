52358c0f990f0c52b4c4c41701c91d920df939de
Axel Anaya <axel_anaya@hotmail.com>
2021-06-20 22:17:51 -0500

    :bug: Resuelto problema de tipado en Inventory

--- src/inventory/schemas/inventory.schema.ts
+++ src/inventory/schemas/inventory.schema.ts
@@ -1,13 +1,13 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document, Schema as mongooseSchema } from 'mongoose';
+import { Document, Types } from 'mongoose';
 import { Product } from 'src/product/schemas/product.schema';
 
 export type InventoryDocument = Inventory & Document;
 
 @Schema()
 export class Inventory {
-  @Prop({ type: mongooseSchema.Types.ObjectId, ref: 'Product' })
-  product: Product;
+  @Prop({ type: Types.ObjectId, ref: 'Product' })
+  product: Product | Types.ObjectId;
 
   @Prop({ required: true })
   stock: number;
