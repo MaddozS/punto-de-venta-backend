7468161282be570a3c98567d8ea9d11e57746dac
Axel Anaya <axel_anaya@hotmail.com>
2021-06-21 17:38:22 -0500

    :sparkles: Implementado métodos del controllador de Supplier

--- src/supplier/supplier.controller.ts
+++ src/supplier/supplier.controller.ts
@@ -1,4 +1,60 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateSupplierDTO } from './dto/supplier.dto';
+import { Supplier } from './schemas/supplier.schema';
+import { SupplierService } from './supplier.service';
 
 @Controller('supplier')
-export class SupplierController {}
+export class SupplierController {
+  constructor(private readonly supplierService: SupplierService) {}
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  async getSuppliers(): Promise<Supplier[]> {
+    const suppliers = await this.supplierService.getAll();
+
+    return suppliers;
+  }
+
+  @Get(':id')
+  @HttpCode(HttpStatus.OK)
+  async getSupplier(@Param() params): Promise<Supplier> {
+    const suppliers = await this.supplierService.getOne(params.id);
+
+    return suppliers;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async createSupplier(@Body() createSupplierDTO: CreateSupplierDTO): Promise<Supplier> {
+    let supplier = await this.supplierService.createOne(createSupplierDTO).catch((error) => {
+      throw new BadRequestException(error, 'No se ha podido crear el proveedor');
+    });
+
+    return supplier;
+  }
+
+  @Put(':id')
+  @HttpCode(HttpStatus.OK)
+  async updateSupplier(@Param() params, @Body() createSupplierDTO: CreateSupplierDTO): Promise<Supplier> {
+    const updatedSupplier = await this.supplierService.updateOne(params.id, createSupplierDTO);
+
+    return updatedSupplier;
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async deleteSupplier(@Param() params): Promise<void> {
+    await this.supplierService.deleteOne(params.id);
+  }
+}
