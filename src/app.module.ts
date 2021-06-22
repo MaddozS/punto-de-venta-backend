8a586641cb3d4069beef514314bb8859c1bec7b6
Alan Morales <alanmoraales845@gmail.com>
2021-06-21 22:21:11 -0500

    modify env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule, ConfigType } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { SuppliersModule } from './suppliers/suppliers.module';
-import config from 'src/shared/config';
 
 @Module({
-  imports: [
-    process.env.NODE_ENV !== 'production'
-      ? ConfigModule.forRoot({
-          envFilePath: `.env.${process.env.NODE_ENV}`,
-        })
-      : null,
-    ProductModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://admin:aB31ZXDu4gtqPkB6@cluster0.5rkcb.mongodb.net/tiendita?retryWrites=true&w=majority',
-    ),
-    SuppliersModule,
-  ],
+  imports: [ConfigModule.forRoot(), ProductModule, MongooseModule.forRoot(`${process.env.MONGO_URI}`), SuppliersModule],
   controllers: [AppController],
   providers: [AppService],
 })
