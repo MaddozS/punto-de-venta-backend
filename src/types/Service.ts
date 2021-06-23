91ef8cd727d8f370122b56a2328b08070e688208
Axel Anaya <axel_anaya@hotmail.com>
2021-06-22 19:24:46 -0500

    :recycle: Cambios a la interfaz Service

--- src/types/Service.ts
+++ src/types/Service.ts
@@ -1,11 +1,13 @@
-export interface Service<T, DTO> {
+export interface Service<T, CreateDTO> {
   getAll(): Promise<T[]>;
 
   getOne(id: string): Promise<T>;
 
-  createOne(dto: DTO): Promise<T>;
+  createOne(dto: CreateDTO): Promise<T>;
 
-  updateOne(id: string, dto: DTO): Promise<T>;
+  // Si defino en UpdateDTO, entonces el parametro DTO debe ser de tipo UpdateDTO
+  // si no está definido, entonces el parametro es de tipo CreateDTO
+  updateOne<UpdateDTO = CreateDTO>(id: string, dto: UpdateDTO): Promise<T>;
 
   deleteOne(id: string): Promise<T>;
 }
