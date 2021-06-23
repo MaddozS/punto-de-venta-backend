991dcda164a9e6ca8c462f8ad9b4a05385b09b9e
Axel Anaya <axel_anaya@hotmail.com>
2021-06-23 13:14:31 -0500

    :sparkles: Definido esquema de Promotion

--- src/promotion/schemas/promotion.schema.ts
+++ src/promotion/schemas/promotion.schema.ts
@@ -1,9 +1,29 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
+import { ProductQty, ProductQtySchema } from './productQty.shema';
 
 export type PromotionDocument = Promotion & Document;
 
 @Schema()
 export class Promotion {
+  @Prop({ type: [ProductQtySchema], default: [] })
+  products: Types.Array<ProductQty>;
 
+  @Prop({ required: true, unique: true })
+  code: string;
+
+  @Prop({ required: true })
+  amount: number;
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({ required: true })
+  startDate: Date;
+
+  @Prop({ required: true })
+  expiringDate: Date;
+
+  @Prop({ default: new Date() })
+  createdAt: Date;
 }
