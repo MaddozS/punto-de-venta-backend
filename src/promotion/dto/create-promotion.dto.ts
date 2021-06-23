abb36838f1a8f7b0569bb3e54ddee58e668a8db5
Axel Anaya <axel_anaya@hotmail.com>
2021-06-23 11:58:13 -0500

    :sparkles: Creado DTOs para Promotion

---
+++ src/promotion/dto/create-promotion.dto.ts
@@ -0,0 +1,26 @@
+import { IsArray, IsDate, IsNotEmpty, IsString } from 'class-validator';
+import { CreateProductQtyDTO } from './create-product-qty.dto';
+
+export class CreatePromotionDTO {
+  @IsArray()
+  readonly products?: CreateProductQtyDTO[]; //vacio significa que no hay condicion de productos
+
+  @IsNotEmpty()
+  readonly amount: number;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly description: string;
+
+  @IsNotEmpty()
+  @IsDate()
+  readonly startDate: Date;
+
+  @IsNotEmpty()
+  @IsDate()
+  readonly expiringDate: Date;
+
+  @IsNotEmpty()
+  @IsDate()
+  readonly createdAd: Date;
+}
