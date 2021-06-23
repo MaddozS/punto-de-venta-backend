30f230ddb4e9d6176380e3a1d387334171909eec
Axel Anaya <axel_anaya@hotmail.com>
2021-06-23 11:38:05 -0500

    :sparkles: Creado DTO de update inventory

---
+++ src/inventory/dto/update-inventory.dto.ts
@@ -0,0 +1,9 @@
+import { IsNotEmpty, IsNumber } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateInventoryDTO } from './create-inventory.dto';
+
+export class UpdateInventoryDTO extends PartialType(CreateInventoryDTO) {
+  @IsNotEmpty()
+  @IsNumber()
+  readonly stock: number;
+}
