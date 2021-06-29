9ab5c195db98401840692d84978adac8954d0072
Rodrigoh2702 <rodrigoh.gongora@gmail.com>
2021-06-28 22:53:04 -0500

    Finished unit tests for products and suppliers

--- test/unit-test-products.test.js
+++ test/unit-test-products.test.js
@@ -40,4 +40,57 @@ describe('Pruebas unitarias del REST API', () => {
         done();
       });
   });
+
+  it('/products (PUT) Modificar un producto', function (done) {
+    // Se crea el objeto a modificar en la base de datos
+    const modifiedProduct = {
+      name: 'Papas Chilosas',
+      price: 5.5,
+      description: 'Papas muy picosas bolsa 45g',
+    };
+
+    chai
+      // Se manda un request al servidor para encontrar el producto
+      .request(baseUrl)
+      .get('/products')
+      .then(function (res) {
+        // Guardamos el ID del producto a modificar
+        const id = res.body[0]._id;
+        // Hacemos una segunda request para modificar el producto
+        chai
+          .request(baseUrl)
+          .put(`/products/${id}`)
+          .send(modifiedProduct)
+          .end(function (err, res) {
+          // Se verifica la respuesta
+          chai.expect(res).to.have.status(200);
+          chai.expect(res.body).to.have.property('_id');
+          chai.expect(res.body.name).to.equal(modifiedProduct.name);
+          chai.expect(res.body.price).to.equal(modifiedProduct.price);
+          chai.expect(res.body.description).to.equal(modifiedProduct.description);
+          done();
+        });
+      });
+  });
+  
+  it('/products (DELETE) Eliminar un producto', function (done) {
+    chai
+      // Se manda un request al servidor para encontrar el producto a eliminar
+      .request(baseUrl)
+      .get('/products')
+      .then(function (res) {
+        // Se guarda el ID del producto a eliminar
+        const id = res.body[0]._id;
+        // Se manda una segunda request para eliminar el producto
+        chai
+          .request(baseUrl)
+          .delete(`/products/${id}`)
+          .end(function (err, res) {
+          // Se verifica la respuesta
+          chai.expect(res).to.have.status(204);
+          done();
+        });
+      });
+  });
+
 });
--- test/unit-test-suppliers.test.js
+++ test/unit-test-suppliers.test.js
@@ -23,7 +23,7 @@ describe('Pruebas unitarias del REST API', () => {
   it('/suppliers (POST) Insertar un proveedor', function (done) {
     // Se crea el objeto a crear en la base de datos
     const supplier = {
-      name: 'Proveedor nuevo (de prueba)',
+      name: "Proveedor (de prueba)",
     };
 
     chai
@@ -36,6 +36,54 @@ describe('Pruebas unitarias del REST API', () => {
         chai.expect(res.body).to.have.property('_id');
         chai.expect(res.body).to.have.property('productsOffer');
         done();
+      });
+  });
+
+  it('/suppliers (PATCH) Modificar un proveedor', function (done) {
+    // Se crea el objeto a modificar en la base de datos
+    const modifiedSupplier = {
+      name: 'Proveedor modificado (de prueba)',
+    };
+
+    chai
+      // Se manda un request al servidor para encontrar el proveedor
+      .request(baseUrl)
+      .get('/suppliers')
+      .then(function (res) {
+        // Guardamos el ID del proveedor a modificar
+        const id = res.body[0]._id;
+        // Hacemos una segunda request para modificar el proveedor
+        chai
+          .request(baseUrl)
+          .patch(`/suppliers/${id}`)
+          .send(modifiedSupplier)
+          .end(function (err, res) {
+          // Se verifica la respuesta
+          chai.expect(res).to.have.status(200);
+          chai.expect(res.body).to.have.property('_id');
+          chai.expect(res.body.name).to.equal(modifiedSupplier.name);
+          done();
+        });
+      });
+  });
+
+  it('/suppliers (DELETE) Eliminar un proveedor', function (done) {
+    chai
+      // Se manda un request al servidor para encontrar el proveedor a eliminar
+      .request(baseUrl)
+      .get('/suppliers')
+      .then(function (res) {
+        // Se guarda el ID del proveedor a eliminar
+        const id = res.body[0]._id;
+        // Se manda una segunda request para eliminar el proveedor
+        chai
+          .request(baseUrl)
+          .delete(`/suppliers/${id}`)
+          .end(function (err, res) {
+          // Se verifica la respuesta
+          chai.expect(res).to.have.status(200);
+          done();
+        });
       });
   });
 });
